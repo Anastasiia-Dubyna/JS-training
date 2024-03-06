e21594f22418d398336644a119f2183ab37bbd6f
Anastasiia-Dubyna <122117575+Anastasiia-Dubyna@users.noreply.github.com>
2024-03-06 21:25:57 +0200

    update CreateBox

--- createBox/createBox.js
+++ createBox/createBox.js
@@ -1,8 +1,13 @@
 const create = document.querySelector("[data-create]");
+const destroy = document.querySelector("[data-destroy]");
+const destroy1 = document.querySelector("[data-destroy1]");
 const input = document.querySelector(".input");
 const boxes = document.querySelector("#boxes");
+
 create.addEventListener("click", handleCreateClick);
-console.log(boxes);
+destroy.addEventListener("click", handleDelete);
+destroy1.addEventListener("click", handleDeleteLast);
+
 function handleCreateClick() {
   if (input.value === "0" || input.value === "") {
     alert("Choose number from 1 to ..");
@@ -12,11 +17,13 @@ function handleCreateClick() {
 }
 
 function createBoxes(amount) {
+  let boxSize = 30;
   for (let i = 1; i <= amount; i++) {
     boxes.insertAdjacentHTML(
       "beforeend",
-      `<div style="width:30px; height:30px; background:${getRandomHexColor()}"></div>`
+      `<div style="width:${boxSize}px; height:${boxSize}px; background:${getRandomHexColor()}"></div>`
     );
+    boxSize += 10;
   }
 }
 
@@ -24,4 +31,15 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+function handleDelete() {
+  boxes.innerHTML = "";
+}
+
+function handleDeleteLast() {
+  const lastBox = boxes.lastElementChild;
+  if (lastBox) {
+    lastBox.remove();
+  }
 }
